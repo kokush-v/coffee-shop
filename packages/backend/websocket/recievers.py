4af2fe65001b619a18c13a5cea9753f7decc430e
kokush <witalik.logoyda@gmail.com>
2025-02-23 01:36:52 +0200

    refactor(websocket): improve order event handling and user group management

--- packages/backend/websocket/recievers.py
+++ packages/backend/websocket/recievers.py
@@ -8,19 +8,28 @@ from shop.models import Order
 
 
 @receiver(post_save, sender=Order)
-def order_status_changed(sender, instance, created, *args,**kwargs,):
+def order_status_changed(sender, instance, created, *args, **kwargs,):
+    order = OrderSerializer(instance).data  # Сериалізуємо замовлення
+    channel_layer = get_channel_layer()
+
     if created:
         return
-    
-    order = OrderSerializer(instance).data
-    
-    print(order)
-    
-    channel_layer = get_channel_layer()
+
     async_to_sync(channel_layer.group_send)(
-        f"user_{instance.user.id}",
+        "admins",
         {
-            'type': 'order_event',
-            'order': order,
+            "type": "order_event",
+            "order": order,
+            "sender": order['user']['username'],
+            "user_id": order['user']['id'],
+        }
+    )
+
+    async_to_sync(channel_layer.group_send)(
+        f"user_{order['user']['id']}",
+        {
+            "type": "order_event",
+            "order": order,
+            "sender": "Система",
         }
     )
